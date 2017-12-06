be0bbc49f49481297599890a6a92947c03d8524a
penghiang <33290649+penghiang@users.noreply.github.com>
2017-12-06 21:05:49 +0800

    seed generation and show loop

--- 50.002 game.mojo
+++ 50.002 game.mojo
@@ -1,7 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <project name="50.002 game" board="Mojo V3" language="Lucid">
   <files>
+    <src>alu_shifter.luc</src>
+    <src>alu_comparator.luc</src>
+    <src>ram.v</src>
+    <src>alu_combined.luc</src>
+    <src>alu_boolean.luc</src>
     <src top="true">mojo_top.luc</src>
+    <src>counter.luc</src>
+    <src>alu_adder.luc</src>
     <ucf lib="true">io_shield.ucf</ucf>
     <ucf lib="true">mojo.ucf</ucf>
     <component>reset_conditioner.luc</component>
---
+++ source/ram.v
@@ -0,0 +1,21 @@
+module ram #(
+    parameter SIZE = 8,  // size of each entry
+    parameter DEPTH = 32  // number of entries
+  )(
+    input clk,                         // clock
+    input [$clog2(DEPTH)-1:0] address, // address to read or write
+    output reg [SIZE-1:0] read_data,   // data read
+    input [SIZE-1:0] write_data,       // data to write
+    input write_en                     // write enable (1 = write)
+  );
+ 
+  reg [SIZE-1:0] ram [DEPTH-1:0];      // memory array
+ 
+  always @(posedge clk) begin
+    read_data <= ram[address];         // read the entry
+ 
+    if (write_en)                      // if we need to write
+      ram[address] <= write_data;      // update that value
+  end
+ 
+endmodule
